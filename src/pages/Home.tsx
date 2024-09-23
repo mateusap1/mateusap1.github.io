756745ec9e464d19c14ce6909ff1110f64d0f51f
Mateus Santos <santos.mateus@protonmail.com>
2024-09-23 14:06:37 -0300

    completed first projects design

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Presentation from "../components/Presentation";
+import ProjectItem from "../components/ProjectItem";
 
 const backgrounds = [
   "/assets/bgweb3.jpg",
@@ -15,9 +16,10 @@ const backgrounds = [
 
 type RoleSliderProps = {
   onChange: (idx: number) => void;
+  onViewMyWork: (idx: number) => void;
 };
 
-const RoleSlider = ({ onChange }: RoleSliderProps) => {
+const RoleSlider = ({ onChange, onViewMyWork }: RoleSliderProps) => {
   var settings = {
     dots: true,
     arrows: false,
@@ -44,7 +46,7 @@ const RoleSlider = ({ onChange }: RoleSliderProps) => {
             ea commodo consequat.
           </span>
         }
-        onViewMyWork={() => {}}
+        onViewMyWork={() => onViewMyWork(0)}
       />
       <Presentation
         title="I work with AI."
@@ -57,7 +59,7 @@ const RoleSlider = ({ onChange }: RoleSliderProps) => {
             ea commodo consequat.
           </span>
         }
-        onViewMyWork={() => {}}
+        onViewMyWork={() => onViewMyWork(1)}
       />
       <Presentation
         title="I create web3 applications."
@@ -70,7 +72,7 @@ const RoleSlider = ({ onChange }: RoleSliderProps) => {
             ea commodo consequat.
           </span>
         }
-        onViewMyWork={() => {}}
+        onViewMyWork={() => onViewMyWork(2)}
       />
     </Slider>
   );
@@ -78,18 +80,26 @@ const RoleSlider = ({ onChange }: RoleSliderProps) => {
 
 export default () => {
   const [currentPage, setCurrentPage] = useState(0);
-  const ref: MutableRefObject<HTMLDivElement | null> = useRef(null);
+
+  const backgroundRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const web3Ref: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
   const changeBackground = (idx: number) => {
-    if (ref.current == null) return;
+    if (backgroundRef.current == null) return;
 
-    ref.current.style.backgroundImage = `url("${backgrounds[idx]}")`;
+    backgroundRef.current.style.backgroundImage = `url("${backgrounds[idx]}")`;
+  };
+
+  const viewMyWork = (idx: number) => {
+    if (web3Ref.current == null) return;
+
+    web3Ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
   };
 
   return (
     <div className="w-full font-display">
       <div
-        ref={ref}
+        ref={backgroundRef}
         style={{ backgroundImage: `url("${backgrounds[0]}")` }}
         className={`h-screen bg-center bg-cover transition-all duration-1000 ease-in-out`}
       >
@@ -112,15 +122,18 @@ export default () => {
         <div className="h-full flex p-8">
           <div className="z-20 relative h-full w-full flex px-48 py-24">
             <div className="w-full flex flex-col gap-8 text-slate-200">
-              <RoleSlider onChange={(idx) => changeBackground(idx)} />
+              <RoleSlider
+                onViewMyWork={viewMyWork}
+                onChange={(idx) => changeBackground(idx)}
+              />
             </div>
           </div>
         </div>
       </div>
 
       <div className="w-full grid grid-cols-3 p-32">
-        <div className="flex flex-col gap-8 col-span-1 text-white">
-          <span className="font-title font-semibold text-4xl">
+        <div className="flex flex-col h-full items-center justify-center gap-8 col-span-1 text-white p-8">
+          <span ref={web3Ref} className="font-title font-semibold text-4xl">
             I developed web3 projects.
           </span>
           <p className="opacity-60">
@@ -129,27 +142,12 @@ export default () => {
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
-          <img
-            className="w-16"
-            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.iconfinder.com%2Fdata%2Ficons%2Fbitcoin-and-mining%2F44%2Fblockchain-1024.png&f=1&nofb=1&ipt=04802b35c130109ff9bdda83fe33f245976dec8d2068a4eed150be0cae175a42&ipo=images"
-          />
         </div>
-        <div>
-          <div>
-            <div>
-              <img className="w-8" src="assets/icons/github.svg" />
-            </div>
-            <span>Integrating Algolia Search with WordPress Multisite</span>
-            <span>
-              Building a custom multisite compatible WordPress plugin to build
-              global search with Algolia.
-            </span>
-            <div>
-              <span>Algolia</span>
-              <span>WordPress</span>
-              <span>PHP</span>
-            </div>
-          </div>
+        <div className="flex flex-wrap justify-end gap-4 col-span-2 col-start-2 text-white p-8">
+          <ProjectItem />
+          <ProjectItem />
+          <ProjectItem />
+          <ProjectItem />
         </div>
       </div>
     </div>
