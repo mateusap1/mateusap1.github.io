78d689243a191d776bc390be12260d7303af0e6e
Mateus Santos <santos.mateus@protonmail.com>
2024-09-25 13:25:32 -0300

    modularized landing page

---
+++ src/components/Landing.tsx
@@ -0,0 +1,55 @@
+import { MutableRefObject, useRef } from "react";
+import RoleSlider from "./RoleSlider";
+
+const backgrounds = [
+  "/assets/bgweb3.jpg",
+  "/assets/bgai.jpg",
+  "/assets/bgsoft.jpg",
+];
+
+type LandingProps = {
+  goToAboutMe: () => void;
+  goToWork: (idx: number) => void;
+};
+
+export default ({ goToAboutMe, goToWork }: LandingProps) => {
+  const ref: MutableRefObject<HTMLDivElement | null> = useRef(null);
+
+  const changeBackground = (idx: number) => {
+    if (ref.current == null) return;
+
+    ref.current.style.backgroundImage = `url("${backgrounds[idx]}")`;
+  };
+
+  return (
+    <div
+      ref={ref}
+      style={{ backgroundImage: `url("${backgrounds[0]}")` }}
+      className={`h-screen bg-center bg-cover transition-all duration-500 ease-in-out`}
+    >
+      <div className="z-10 absolute h-screen inset-0 bg-black opacity-60"></div>
+      <nav className="z-20 relative p-8">
+        <div className="flex justify-end">
+          <div
+            onClick={goToAboutMe}
+            className="w-fit flex flex-row justify-center hover:opacity-75"
+          >
+            <button className="border border-offblue px-4 py-2 text-offblue">
+              About Me
+            </button>
+            <button className="border border-offblue bg-offblue px-2 py-2">
+              <img className="rotate-90" src="assets/icons/doublearrow.svg" />
+            </button>
+          </div>
+        </div>
+      </nav>
+      <div className="h-full flex p-8">
+        <div className="z-20 relative h-full w-full flex px-48">
+          <div className="w-full flex flex-col gap-8 text-slate-200">
+            <RoleSlider onViewMyWork={goToWork} onChange={changeBackground} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,19 @@
-import { useRef, MutableRefObject, ReactNode, useState } from "react";
-
-import Slider from "react-slick";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { useRef, MutableRefObject } from "react";
 
 import {
   ProjectSectionRight,
   ProjectSectionLeft,
 } from "../components/ProjectSection";
 import B from "../components/B";
-import RoleSlider from "../components/RoleSlider";
 import AboutMe from "../components/AboutMe";
-
-const backgrounds = [
-  "/assets/bgweb3.jpg",
-  "/assets/bgai.jpg",
-  "/assets/bgsoft.jpg",
-];
+import Landing from "../components/Landing";
 
 export default () => {
-  const backgroundRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   const aboutMeRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
 
   const web3Ref: MutableRefObject<HTMLDivElement | null> = useRef(null);
   const aiRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   const softRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
-
-  const changeBackground = (idx: number) => {
-    if (backgroundRef.current == null) return;
-
-    backgroundRef.current.style.backgroundImage = `url("${backgrounds[idx]}")`;
-  };
 
   const viewMyWork = (idx: number) => {
     if (web3Ref.current == null) return;
@@ -54,38 +36,7 @@ export default () => {
 
   return (
     <div className="w-full font-display">
-      <div
-        ref={backgroundRef}
-        style={{ backgroundImage: `url("${backgrounds[0]}")` }}
-        className={`h-screen bg-center bg-cover transition-all duration-500 ease-in-out`}
-      >
-        <div className="z-10 absolute h-screen inset-0 bg-black opacity-60"></div>
-        <nav className="z-20 relative p-8">
-          <div className="flex justify-end">
-            <div
-              onClick={goToAboutMe}
-              className="w-fit flex flex-row justify-center hover:opacity-75"
-            >
-              <button className="border border-offblue px-4 py-2 text-offblue">
-                About Me
-              </button>
-              <button className="border border-offblue bg-offblue px-2 py-2">
-                <img className="rotate-90" src="assets/icons/doublearrow.svg" />
-              </button>
-            </div>
-          </div>
-        </nav>
-        <div className="h-full flex p-8">
-          <div className="z-20 relative h-full w-full flex px-48">
-            <div className="w-full flex flex-col gap-8 text-slate-200">
-              <RoleSlider
-                onViewMyWork={viewMyWork}
-                onChange={(idx) => changeBackground(idx)}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Landing goToAboutMe={goToAboutMe} goToWork={viewMyWork} />
 
       <AboutMe secRef={aboutMeRef} />
 
