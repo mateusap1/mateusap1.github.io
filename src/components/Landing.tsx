3a1c6a60e49316fca1c33a4bd6492c871583d9f6
Mateus Santos <santos.mateus@protonmail.com>
2024-09-27 17:31:53 -0300

    fixed aboutme button

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -28,9 +28,9 @@ export default ({ goToAboutMe, goToWork }: LandingProps) => {
       style={{ backgroundImage: `url("${backgrounds[0]}")` }}
       className={`h-screen bg-center bg-contain bg-no-repeat bg-center md:bg-cover transition-all duration-500 ease-in-out`}
     >
-      <div className="z-10 absolute h-screen inset-0 bg-black opacity-60"></div>
+      <div className="absolute h-screen inset-0 bg-black opacity-60"></div>
       <Navbar goToAboutMe={goToAboutMe} />
-      <div className="absolute inset-0 flex items-center px-8 md:p-8 z-20">
+      <div className="absolute inset-0 flex items-center px-8 md:p-8 z-10">
         <div className="w-full md:px-48">
           <div className="w-full flex flex-col gap-8 text-slate-200">
             <RoleSlider onViewMyWork={goToWork} onChange={changeBackground} />
