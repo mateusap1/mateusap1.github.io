e4d4da9090d9b96765819e6dfdc8743209fffd2b
Mateus Santos <santos.mateus@protonmail.com>
2024-09-28 11:48:38 -0300

    reduced size of aboutme button mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,16 +10,16 @@ export default ({ goToAboutMe }: NavbarProps) => (
         onClick={goToAboutMe}
         className="group w-fit flex flex-row justify-center hover:cursor-pointer"
       >
-        <button className="border group-hover:bg-offblue group-hover:text-white group-hover:font-semibold border-offblue px-4 py-2 text-offblue">
+        <button className="border group-hover:bg-offblue group-hover:text-white group-hover:font-semibold border-offblue text-xs md:text-base px-2 md:px-4 md:py-2 text-offblue">
           About Me
         </button>
         <button className="border border-offblue group-hover:bg-transparent bg-offblue px-2 py-2">
           <img
-            className="rotate-90 group-hover:hidden"
+            className="rotate-90 group-hover:hidden w-4 md:w-6"
             src="/portifolio/assets/icons/doublearrow.svg"
           />
           <img
-            className="hidden group-hover:inline rotate-90"
+            className="hidden group-hover:inline rotate-90 w-4 md:w-6"
             src="/portifolio/assets/icons/doublearrow_offblue.svg"
           />
         </button>
